9e003c09cd9a21acd8cd8e07e84a91611801177b
Ryoh Akiyoshi <ryo614@gmail.com>
2021-04-27 13:53:29 +0900

    Add method to import all accounts

--- src/importer.ts
+++ src/importer.ts
@@ -1,32 +1,59 @@
 import { AWSClient } from "./AWSClient";
-import {
-  AccountAssignment,
-} from "@aws-sdk/client-sso-admin";
+import { AccountAssignment } from "@aws-sdk/client-sso-admin";
 
 export class Importer {
   private client: AWSClient;
 
-  constructor (region: string) {
+  constructor(region: string) {
     this.client = new AWSClient(region);
   }
 
-  public async fetchAllAssignments(accountIds: string[]): Promise<AccountAssignment[]> {
+  public async fetchAllAssignments(): Promise<AccountAssignment[]> {
+    console.log("Fetching accounts in AWS Organizations.");
+    const accounts = (await this.client.listAccounts()).map(
+      (account) => account.Id!
+    );
+    console.log(
+      "Fetched accounts. Assignments in accounts below will be imported."
+    );
+    console.log(accounts);
+
+    return this.fetchAssignments(accounts);
+  }
+
+  public async fetchAssignments(
+    accountIds: string[]
+  ): Promise<AccountAssignment[]> {
     const instanceArn = await this.client.getInstanceArn();
-    const permissionSets: string[] = await this.client.listPermissionSetArns(instanceArn);
+    const permissionSets: string[] = await this.client.listPermissionSetArns(
+      instanceArn
+    );
     const allAssignment: AccountAssignment[] = [];
     for await (const accountId of accountIds) {
-      allAssignment.push(...await this.fetchAssingmentsByAccountId(accountId, instanceArn, permissionSets))
+      const accountAssignments = await this.fetchAssingmentsByAccountId(
+        accountId,
+        instanceArn,
+        permissionSets
+      );
+      allAssignment.push(...accountAssignments);
     }
     return allAssignment;
   }
 
-  public async fetchAssingmentsByAccountId(accountId: string, instanceArn: string, permissionSets: string[]): Promise<AccountAssignment[]> {
+  public async fetchAssingmentsByAccountId(
+    accountId: string,
+    instanceArn: string,
+    permissionSets: string[]
+  ): Promise<AccountAssignment[]> {
     const accountAssignments = [];
     console.debug(`Fetching account assignments for Account: ${accountId}...`);
     for await (const permissionSet of permissionSets) {
-      accountAssignments.push(
-        ...await this.client.listAccountAssignments(accountId, instanceArn, permissionSet)
-      )
+      const accountPermissionSetAssignments = await this.client.listAccountAssignments(
+        accountId,
+        instanceArn,
+        permissionSet
+      );
+      accountAssignments.push(...accountPermissionSetAssignments);
     }
     console.debug(`Fetched ${accountAssignments.length} account assignments.`);
     return accountAssignments;
